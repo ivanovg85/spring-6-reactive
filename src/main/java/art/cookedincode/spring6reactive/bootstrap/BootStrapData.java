dcf7e42580b4eb1e8b42929d801e15b9f28bcf41
Georgi Ivanov <georgi.ivanov85@hotmail.com>
2025-10-05 02:33:17 +0300

    add database configuration, entities and tests

---
+++ src/main/java/art/cookedincode/spring6reactive/bootstrap/BootStrapData.java
@@ -0,0 +1,64 @@
+package art.cookedincode.spring6reactive.bootstrap;
+
+import art.cookedincode.spring6reactive.domain.Beer;
+import art.cookedincode.spring6reactive.repositories.BeerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Arrays;
+
+/**
+ * Created by Georgi Ivanov
+ */
+@RequiredArgsConstructor
+@Component
+public class BootStrapData implements CommandLineRunner {
+
+    private final BeerRepository beerRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        loadBeerData();
+    }
+
+    private void loadBeerData() {
+        beerRepository.count().subscribe(count -> {
+            if (count == 0) {
+                Beer beer1 = Beer.builder()
+                        .beerName("Galaxy Cat")
+                        .beerStyle("Pale Ale")
+                        .upc("12356")
+                        .price(new BigDecimal("12.99"))
+                        .quantityOnHand(122)
+                        .createdDate(LocalDateTime.now())
+                        .lastModifiedDate(LocalDateTime.now())
+                        .build();
+
+                Beer beer2 = Beer.builder()
+                        .beerName("Crank")
+                        .beerStyle("Pale Ale")
+                        .upc("12356222")
+                        .price(new BigDecimal("11.99"))
+                        .quantityOnHand(392)
+                        .createdDate(LocalDateTime.now())
+                        .lastModifiedDate(LocalDateTime.now())
+                        .build();
+
+                Beer beer3 = Beer.builder()
+                        .beerName("Sunshine City")
+                        .beerStyle("Ipa")
+                        .upc("12356")
+                        .price(new BigDecimal("13.99"))
+                        .quantityOnHand(144)
+                        .createdDate(LocalDateTime.now())
+                        .lastModifiedDate(LocalDateTime.now())
+                        .build();
+
+                beerRepository.saveAll(Arrays.asList(beer1, beer2, beer3)).subscribe();
+            }
+        });
+    }
+}
---
+++ src/test/java/art/cookedincode/spring6reactive/repositories/BeerRepositoryTest.java
@@ -0,0 +1,39 @@
+package art.cookedincode.spring6reactive.repositories;
+
+import art.cookedincode.spring6reactive.config.DatabaseConfig;
+import art.cookedincode.spring6reactive.domain.Beer;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.r2dbc.DataR2dbcTest;
+import org.springframework.context.annotation.Import;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by Georgi Ivanov
+ */
+@DataR2dbcTest
+@Import(DatabaseConfig.class)
+class BeerRepositoryTest {
+
+    @Autowired
+    BeerRepository beerRepository;
+
+    @Test
+    void saveNewBeer() {
+        beerRepository.save(getTestBeer())
+                .subscribe(System.out::println);
+    }
+
+    Beer getTestBeer() {
+        return Beer.builder()
+                .beerName("Space Dust")
+                .beerStyle("IPA")
+                .price(BigDecimal.TEN)
+                .quantityOnHand(12)
+                .upc("641354")
+                .build();
+    }
+}
