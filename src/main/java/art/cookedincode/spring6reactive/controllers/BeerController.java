832ce0907cb49e684782184d6dd0e6bd4a5a1d03
Georgi Ivanov <georgi.ivanov85@hotmail.com>
2025-10-05 03:34:25 +0300

    add Beer controller

---
+++ src/main/java/art/cookedincode/spring6reactive/controllers/BeerController.java
@@ -0,0 +1,21 @@
+package art.cookedincode.spring6reactive.controllers;
+
+import art.cookedincode.spring6reactive.model.BeerDTO;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+/**
+ * Created by Georgi Ivanov
+ */
+@RestController
+public class BeerController {
+
+    public static final String BEER_PATH = "/api/v2/beer";
+
+    @GetMapping(BEER_PATH)
+    Flux<BeerDTO> listBeers() {
+        return Flux.just(BeerDTO.builder().id(1).build(),
+                BeerDTO.builder().id(2).build());
+    }
+}
